a5e9df6d7706267e264c834bee6765cd48713ce2
erinleary <163932981+erinleary@users.noreply.github.com>
2024-06-10 19:20:49 -0400

    06/10 until break
    Notes on basic boolean language

---
+++ class3-bool/practice operators_boolean_comments.py
@@ -0,0 +1,82 @@
+
+#Script to convert temperature
+
+fahrenheit=89 #temp to convert
+
+#Conversion Formula
+celsius =(fahrenheit-32)*5/9
+
+print(celsius) #output
+
+'''Shortcut Operators'''
+#Add 5 to me
+age=25
+age +=5 #adding 5 via shortcut operator
+print (age)
+
+
+#Add to 2024
+year=2024
+year +=10
+print (year)
+
+#subtracting 20
+num_one=55
+num_one -=20
+print (num_one)
+
+#subtracting 15
+num_two =11
+num_two -=15
+print (num_two)
+
+#trying multiplication 
+my_value =9
+my_value *=3
+print(my_value)
+
+#multiply by10
+mileage=15
+mileage *=10
+print (mileage)
+
+#divide by 2
+pizza_slices =8
+pizza_slices /=2
+print (pizza_slices)
+
+#Divide by 7 
+fees = 8.90
+fees /=7
+print(fees)
+
+#Raise to the 3rd Power
+num_three=6
+num_three **=3
+print(num_three)
+
+#raise to the 2nd Power
+data=16
+data **=2
+print(data)
+
+#integer division, how many times does 3 go into 16
+val_one=16
+val_one /=3
+print(val_one)
+
+#Integer divide by 4
+val_two=9
+val_two /=4
+print(val_two)
+
+#Modulus we use often to find if a value is odd or even
+#Find the remainder if divided by 3  (%)
+val_three=10
+val_three %=3
+print(val_three)
+
+#find remainder if divided by 5 (%)
+val_four=14
+val_four %=5
+print(val_four)
---
+++ class3-bool/students.py
@@ -0,0 +1,12 @@
+# Aliases
+import pandas as pd
+
+# My Student Dictionary
+cooking_class_dict={"Name": ['Sarah', 'Joe', 'Alice'],
+                    "favorite_food":['Pizza', 'Ice Cream', 'Mango']}
+
+
+# what does this look like
+students_df=pd.DataFrame(cooking_class_dict)
+print(students_df)
+
